025c2cace129ed5bd5f46430a984e7873f2fbfb8
Cale Bybee <bybeecale@gmail.com>
2021-09-16 18:02:40 -0600

    intro buildout

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,9 @@ import "./intro.min.css"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <div className="left-intro"></div>
+      <div className="left-intro">
+        <div className="img-container"></div>
+      </div>
       <div className="right-intro"></div>
     </div>
   )
