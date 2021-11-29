5fabfdc4934c7a816c2813cfc2d6e225fb2e4ca4
Cale Bybee <bybeecale@gmail.com>
2021-11-29 15:44:50 -0700

    added changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1250,
       backSpeed: 20,
-      strings: ["Software Developer", "Student", "Gamer", "Anime Enthusiast", "Golfer" ]
+      strings: ["Software Developer", "Python Fanatic", "Student", "Gamer", "Anime Enthusiast", "Golfer" ]
     });
   }, []);
 
@@ -18,11 +18,22 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="intro-wrapper">
         <h2>Hi there, I'm</h2>
-          <h1>Cale Bybee</h1>
-          <h3><span ref={textRef}></span></h3>
-          <a href="#portfolio">
-            <img src="assets/down-arrow-5.png" alt="down-arrow"></img>
-          </a>
+        <h1>Cale Bybee</h1>
+        <h3><span ref={textRef}></span></h3>
+
+        <p>
+          I'm a software engineer specialized in backend development, working on my frontend skills (it's a work in progress).
+        </p>
+        <p>
+          Currently, I'm focused on building API integrations for <a href="https://www.synergyscience.com/" className = "company-name">Synergy Science</a>.
+        </p>
+        <p className="dev-message">
+          If you're seeing this message, my site is still under development. Check back often for updates :D
+        </p>
+
+        <a href="#portfolio">
+          <img src="assets/white-arrow-nb.png" alt="down-arrow"></img>
+        </a>
       </div>
     </div>
   )
