a802198cba21b787c67a85af7279cc55f94fc028
Cale Bybee <bybeecale@gmail.com>
2021-11-29 13:40:36 -0700

    adjusted mediaquery

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PortfolioList from "../portfolioList/portfolioList";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.min.css";
 
 export default function Portfolio() {
