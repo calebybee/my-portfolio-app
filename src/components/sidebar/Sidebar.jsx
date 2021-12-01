8b4a2be3f3d7dd8f85d5a1a584c6380b584b7b23
Cale Bybee <bybeecale@gmail.com>
2021-12-01 16:28:36 -0700

    added email sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,24 +6,34 @@ import "./sidebar.min.css";
 
 export default function Sidebar() {
     return (
-      <div className="left-sidebar">
-        <ul className="left-sidebar-list">
-          <li className="sidebar-icons">
-            <a className="sidebar-links" href="https://github.com/calebybee" target="_blank" rel="noopener noreferrer">
-              <GitHubIcon />
-            </a>
-          </li>
-          <li className="sidebar-icons">
-            <a className="sidebar-links" href="https://www.linkedin.com/in/calebybee/" target="_blank" rel="noopener noreferrer">
-              <LinkedInIcon />
-            </a>
-          </li>
-          <li className="sidebar-icons">
-            <a className="sidebar-links" href="https://www.codewars.com/users/calebybee/" target="_blank" rel="noopener noreferrer">
-              <CodeIcon />
-            </a>
-          </li>
-        </ul>
+      <div class="sidebar">
+        <div className="left-sidebar">
+          <ul className="left-sidebar-list">
+            <li className="sidebar-list-item">
+              <a className="sidebar-links" href="https://github.com/calebybee" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon />
+              </a>
+            </li>
+            <li className="sidebar-list-item">
+              <a className="sidebar-links" href="https://www.linkedin.com/in/calebybee/" target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon />
+              </a>
+            </li>
+            <li className="sidebar-list-item">
+              <a className="sidebar-links" href="https://www.codewars.com/users/calebybee/" target="_blank" rel="noopener noreferrer">
+                <CodeIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="right-sidebar">
+            <ul className="right-sidebar-list">
+              <li className="right-sidebar-list-item" id="vertical-email">
+                <a className="email-link" href="mailto:bybeecale@gmail.com?subject=Found you from hired." target="_blank" rel="noopener noreferrer">bybeecale@gmail.com</a>
+              </li>
+            </ul>
+        </div>
       </div>
+      
     )
 }
