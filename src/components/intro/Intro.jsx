2973c340661650d55d21d4c98199dbaab1b94ee6
Cale Bybee <bybeecale@gmail.com>
2021-12-08 09:44:54 -0700

    fixed arrow placement, more scss fixes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -22,18 +22,16 @@ export default function Intro() {
         <h3><span ref={textRef}></span></h3>
 
         <p>
-          I'm a software engineer specialized in backend development, working on my frontend skills (it's a work in progress).
-        </p>
-        <p>
-          Currently, I'm focused on building API integrations for <a href="https://www.synergyscience.com/" className = "company-name">Synergy Science</a>.
+          I'm a software engineer specialized in backend development, working on my frontend skills (it's a work in progress).<br></br>Currently, I'm focused on building REST API integrations for <a href="https://www.synergyscience.com/" className = "company-name">Synergy Science</a>.
         </p>
         <p className="dev-message">
           If you're seeing this message, my site is still under development. Check back often for updates :D
         </p>
-
-        <a className="arrow" href="#portfolio">
-          <img src="assets/white-arrow-nb.png" alt="down-arrow"></img>
-        </a>
+        <div className="arrow-wrapper">
+          <a className="arrow" href="#portfolio">
+            <img src="assets/white-arrow-nb.png" alt="down-arrow"></img>
+          </a>
+        </div>
       </div>
     </div>
   )
